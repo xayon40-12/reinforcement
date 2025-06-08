fbc796bcbd6d61828b36fcb26a31c21e112f5146
xayon40-12 <touroux.nathan@gmail.com>
2025-06-08 20:03:19 +0200

    add example for xor function, not working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use reinforcement::network::{Layer, Layers, Network};
+
 fn main() {
-    println!("Hello, world!");
+    let mut net: Layers<2, 3, 1, Layers<3, 3, 1, Layer<3, 1>>> = Default::default();
+    net.train(
+        10000,
+        1e-2,
+        &[
+            ([0.0, 0.0], [0.0]),
+            ([0.0, 1.0], [1.0]),
+            ([1.0, 1.0], [0.0]),
+            ([1.0, 0.0], [1.0]),
+        ],
+    );
 }
--- src/network.rs
+++ src/network.rs
@@ -1,13 +1,37 @@
 pub type Float = f64;
 
-pub trait Network<I, O> {
-    fn forward(&mut self, input: I) -> O;
+pub trait Network<const NI: usize, const NO: usize> {
+    fn forward(&mut self, input: [Float; NI]) -> [Float; NO];
     /// The parameter $r$ is the ratio $0 < r <= 1$ for the relaxation of the update of the gradient. A value of $r = 1$ correspond to keep only the new value of the gradient, whereas $r = 0.5$ will average the new and last value.
-    fn update_gradient(&mut self, r: Float, delta: O) -> I;
+    fn update_gradient(&mut self, r: Float, delta: [Float; NO]) -> [Float; NI];
     fn reset_gradient(&mut self);
     fn apply_gradient(&mut self, alpha: Float);
+    fn train(
+        &mut self,
+        iterations: usize,
+        alpha: Float,
+        training_data: &[([Float; NI], [Float; NO])],
+    ) {
+        for i in 0..iterations {
+            let mut error = 0.0;
+            for &(inputs, targets) in training_data.iter() {
+                let outputs = self.forward(inputs);
+                if i % (iterations / 10) == 0 {
+                    println!("{outputs:?}");
+                }
+                let delta = std::array::from_fn(|i| outputs[i] - targets[i]);
+                error += delta.iter().map(|d| d * d).sum::<Float>();
+                self.update_gradient(1.0, delta);
+                self.apply_gradient(alpha);
+            }
+            if i % (iterations / 10) == 0 {
+                println!("{i:>10}: {error}");
+            }
+        }
+    }
 }
 
+#[derive(Copy, Clone)]
 pub enum Activation {
     Relu,
     Sigmoid(Float),
@@ -40,7 +64,18 @@ pub struct Layer<const NI: usize, const NO: usize> {
     inputs: [Float; NI],
 }
 
-impl<const NI: usize, const NO: usize> Network<[Float; NI], [Float; NO]> for Layer<NI, NO> {
+impl<const NI: usize, const NO: usize> Default for Layer<NI, NO> {
+    fn default() -> Self {
+        Self {
+            weights: [([0.0; NI], Activation::Relu); NO],
+            gradient: [[0.0; NI]; NO],
+            activations: [0.0; NO],
+            inputs: [0.0; NI],
+        }
+    }
+}
+
+impl<const NI: usize, const NO: usize> Network<NI, NO> for Layer<NI, NO> {
     fn forward(&mut self, input: [Float; NI]) -> [Float; NO] {
         self.inputs = input;
         std::array::from_fn(|o| {
@@ -53,14 +88,14 @@ impl<const NI: usize, const NO: usize> Network<[Float; NI], [Float; NO]> for Lay
             activation.apply(self.activations[o])
         })
     }
-    fn update_gradient(&mut self, ratio_new: Float, deltas_in: [Float; NO]) -> [Float; NI] {
+    fn update_gradient(&mut self, r: Float, deltas_in: [Float; NO]) -> [Float; NI] {
         let mut delta_out = [0.0; NI];
         for (o, gs) in (0..).zip(self.gradient.iter_mut()) {
             let (ws, activation) = &self.weights[o];
             let delta = deltas_in[o] * activation.derivative(self.activations[o]);
             for (i, g) in (0..).zip(gs) {
                 delta_out[i] += delta * ws[i];
-                *g = (1.0 - ratio_new) * *g + ratio_new * delta * self.inputs[i];
+                *g = (1.0 - r) * *g + r * delta * self.inputs[i];
             }
         }
         delta_out
@@ -75,26 +110,27 @@ impl<const NI: usize, const NO: usize> Network<[Float; NI], [Float; NO]> for Lay
             .for_each(|((ws, _), gs)| {
                 ws.iter_mut()
                     .zip(gs.iter())
-                    .for_each(|(w, g)| *w += alpha * *g)
+                    .for_each(|(w, g)| *w -= alpha * *g)
             });
     }
 }
 
-pub struct JoinLayers<const NI: usize, const NH: usize, const NO: usize> {
+#[derive(Default)]
+pub struct Layers<const NI: usize, const NH: usize, const NO: usize, O: Network<NH, NO>> {
     layer_in: Layer<NI, NH>,
-    layer_out: Layer<NH, NO>,
+    layer_out: O,
 }
 
-impl<const NI: usize, const NH: usize, const NO: usize> Network<[Float; NI], [Float; NO]>
-    for JoinLayers<NI, NH, NO>
+impl<const NI: usize, const NH: usize, const NO: usize, O: Network<NH, NO>> Network<NI, NO>
+    for Layers<NI, NH, NO, O>
 {
     fn forward(&mut self, input: [Float; NI]) -> [Float; NO] {
         self.layer_out.forward(self.layer_in.forward(input))
     }
 
-    fn update_gradient(&mut self, ratio_new: Float, delta: [Float; NO]) -> [Float; NI] {
+    fn update_gradient(&mut self, r: Float, delta: [Float; NO]) -> [Float; NI] {
         self.layer_in
-            .update_gradient(ratio_new, self.layer_out.update_gradient(ratio_new, delta))
+            .update_gradient(r, self.layer_out.update_gradient(r, delta))
     }
     fn reset_gradient(&mut self) {
         self.layer_in.reset_gradient();
