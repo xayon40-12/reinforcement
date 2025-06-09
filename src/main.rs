99dc3b9516f9d0ae62eb4ac3c2960c27c996988d
xayon40-12 <touroux.nathan@gmail.com>
2025-06-09 11:08:04 +0200

    first attempt for reinforcement

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use reinforcement::network::{LS, Layer, Layers, Network};
+use reinforcement::network::{Float, Layer, Layers, Network, Reinforcement};
 
 fn add() {
+    //TODO use identity for activation for this case
     let mut net: Layers<2, 3, Layer<3, 1>> = Default::default();
     net.train(
         1000,
@@ -17,7 +18,7 @@ fn add() {
 }
 
 fn xor() {
-    let mut net: LS<2, 2, Layer<2, 1>> = Default::default();
+    let mut net: Layers<2, 2, Layer<2, 1>> = Default::default();
     net.train(
         100,
         1e-1,
@@ -30,7 +31,52 @@ fn xor() {
     );
 }
 
+pub fn distance(a: [Float; 2], b: [Float; 2]) -> Float {
+    ((a[0] - b[0]).powi(2) + (a[1] - b[1]).powi(2)).sqrt()
+}
+
+fn reinforcement() {
+    let mut net: Reinforcement<4, 2, Layers<4, 10, Layers<10, 10, Layer<10, 2>>>> =
+        Default::default();
+
+    let alpha = 1e-1;
+    let target = [10.0, -7.0];
+    let start = [0.0; 2];
+    let max_ticks = 1000;
+    let nb_reinforcement = 1000usize;
+
+    let width = nb_reinforcement.ilog10() as usize;
+    let mut best_distance = distance(start, target);
+    for i in 0..nb_reinforcement {
+        let mut pos = start;
+        let mut ticks = 0;
+        let mut d = 0.0;
+        net.reinforce(1e-2, 1.0, |net| {
+            let input = [pos[0], pos[1], target[0], target[1]];
+            let output = net.forward(input);
+            pos[0] += output[0];
+            pos[1] += output[1];
+            d = distance(target, pos);
+            ticks += 1;
+            if ticks > max_ticks {
+                if d < best_distance {
+                    best_distance = d;
+                    Some(alpha)
+                } else {
+                    Some(-alpha)
+                }
+            } else {
+                None
+            }
+        });
+        if i % (nb_reinforcement / 10) == 0 {
+            println!("{i:<width$}: {d:.2e}");
+        }
+    }
+}
+
 fn main() {
     add();
     xor();
+    reinforcement();
 }
--- src/network.rs
+++ src/network.rs
@@ -1,9 +1,10 @@
 pub type Float = f64;
 
 pub trait JoinNetwork<const NI: usize> {}
-
-pub trait Network<const NI: usize, const NO: usize>: JoinNetwork<NI> {
+pub trait ForwardNetwork<const NI: usize, const NO: usize>: JoinNetwork<NI> {
     fn forward(&mut self, input: [Float; NI]) -> [Float; NO];
+}
+pub trait Network<const NI: usize, const NO: usize>: ForwardNetwork<NI, NO> {
     /// The parameter $r$ is the ratio $0 < r <= 1$ for the relaxation of the update of the gradient. A value of $r = 1$ correspond to keep only the new value of the gradient, whereas $r = 0.5$ will average the new and last value.
     fn update_gradient(&mut self, r: Float, delta: [Float; NO]) -> [Float; NI];
     fn reset_gradient(&mut self);
@@ -23,7 +24,7 @@ pub trait Network<const NI: usize, const NO: usize>: JoinNetwork<NI> {
                     print!("{outputs:?} ");
                 }
                 let normalization = (2.0 * NO as Float).recip();
-                let delta = std::array::from_fn(|i| normalization * (outputs[i] - targets[i]));
+                let delta = std::array::from_fn(|i| normalization * (targets[i] - outputs[i]));
                 error += delta.iter().map(|d| d * d).sum::<Float>();
                 self.update_gradient(1.0, delta);
                 self.apply_gradient(alpha);
@@ -34,7 +35,6 @@ pub trait Network<const NI: usize, const NO: usize>: JoinNetwork<NI> {
         }
     }
 }
-
 pub trait Activation {
     fn apply(x: Float) -> Float;
     fn derivative(x: Float) -> Float;
@@ -42,9 +42,9 @@ pub trait Activation {
 
 #[derive(Copy, Clone, Default, Debug)]
 pub enum Activations {
-    #[default]
     Id,
     Relu,
+    #[default]
     Sigmoid,
 }
 
@@ -54,7 +54,7 @@ impl Activations {
         match self {
             Id => input,
             Relu => input.max(0.0),
-            Sigmoid => (1.0 + (-input).exp()).recip(),
+            Sigmoid => 2.0 * (1.0 + (-input).exp()).recip() - 1.0,
         }
     }
     fn derivative(&self, input: Float) -> Float {
@@ -63,8 +63,8 @@ impl Activations {
             Id => 1.0,
             Relu => input.signum().max(0.0),
             Sigmoid => {
-                let sig = self.apply(input);
-                sig * (1.0 - sig)
+                let e = (-input).exp();
+                2.0 * e / (1.0 + e).powi(2)
             }
         }
     }
@@ -81,7 +81,7 @@ pub struct Layer<const NI: usize, const NO: usize> {
 impl<const NI: usize, const NO: usize> Default for Layer<NI, NO> {
     fn default() -> Self {
         Self {
-            weights: [([1e-1; NI], Activations::Id); NO], // WARNING: this 1e-3 should be a random number
+            weights: [([1e-1; NI], Activations::default()); NO], // WARNING: this 1e-3 should be a random number
             gradient: [[0.0; NI]; NO],
             activations: [0.0; NO],
             inputs: [0.0; NI],
@@ -90,7 +90,7 @@ impl<const NI: usize, const NO: usize> Default for Layer<NI, NO> {
 }
 
 impl<const NI: usize, const NO: usize> JoinNetwork<NI> for Layer<NI, NO> {}
-impl<const NI: usize, const NO: usize> Network<NI, NO> for Layer<NI, NO> {
+impl<const NI: usize, const NO: usize> ForwardNetwork<NI, NO> for Layer<NI, NO> {
     fn forward(&mut self, input: [Float; NI]) -> [Float; NO] {
         self.inputs = input;
         std::array::from_fn(|o| {
@@ -103,6 +103,8 @@ impl<const NI: usize, const NO: usize> Network<NI, NO> for Layer<NI, NO> {
             activation.apply(self.activations[o])
         })
     }
+}
+impl<const NI: usize, const NO: usize> Network<NI, NO> for Layer<NI, NO> {
     fn update_gradient(&mut self, r: Float, deltas_in: [Float; NO]) -> [Float; NI] {
         let mut delta_out = [0.0; NI];
         for (o, gs) in (0..).zip(self.gradient.iter_mut()) {
@@ -125,7 +127,7 @@ impl<const NI: usize, const NO: usize> Network<NI, NO> for Layer<NI, NO> {
             .for_each(|((ws, _), gs)| {
                 ws.iter_mut()
                     .zip(gs.iter())
-                    .for_each(|(w, g)| *w -= alpha * *g)
+                    .for_each(|(w, g)| *w += alpha * *g)
             });
     }
 }
@@ -138,13 +140,16 @@ pub struct Layers<const NI: usize, const NH: usize, O: JoinNetwork<NH>> {
 pub type LS<const NI: usize, const NH: usize, O> = Layers<NI, NH, O>;
 
 impl<const NI: usize, const NH: usize, O: JoinNetwork<NH>> JoinNetwork<NI> for Layers<NI, NH, O> {}
-impl<const NI: usize, const NH: usize, const NO: usize, O: Network<NH, NO>> Network<NI, NO>
+impl<const NI: usize, const NH: usize, const NO: usize, O: Network<NH, NO>> ForwardNetwork<NI, NO>
     for Layers<NI, NH, O>
 {
     fn forward(&mut self, input: [Float; NI]) -> [Float; NO] {
         self.layer_out.forward(self.layer_in.forward(input))
     }
-
+}
+impl<const NI: usize, const NH: usize, const NO: usize, O: Network<NH, NO>> Network<NI, NO>
+    for Layers<NI, NH, O>
+{
     fn update_gradient(&mut self, r: Float, delta: [Float; NO]) -> [Float; NI] {
         self.layer_in
             .update_gradient(r, self.layer_out.update_gradient(r, delta))
@@ -156,5 +161,62 @@ impl<const NI: usize, const NH: usize, const NO: usize, O: Network<NH, NO>> Netw
     fn apply_gradient(&mut self, alpha: Float) {
         self.layer_in.apply_gradient(alpha);
         self.layer_out.apply_gradient(alpha);
+    }
+}
+
+#[derive(Default)]
+pub struct Reinforcement<const NI: usize, const NO: usize, N: Network<NI, NO>> {
+    network: N,
+    r: Float,
+    emphasis: Float,
+}
+impl<const NI: usize, const NO: usize, N: Network<NI, NO>> Reinforcement<NI, NO, N> {
+    pub fn reinforce(
+        &mut self,
+        r: Float,
+        emphasis: Float, // must be positive, and should me relatively small
+        mut f: impl FnMut(&mut dyn ForwardNetwork<NI, NO>) -> Option<Float>,
+    ) where
+        Self: Sized,
+    {
+        self.r = r;
+        self.emphasis = emphasis;
+        loop {
+            let alpha = f(self);
+            if let Some(alpha) = alpha {
+                self.apply_gradient(alpha);
+                self.reset_gradient();
+                break;
+            }
+        }
+    }
+}
+impl<const NI: usize, const NO: usize, N: Network<NI, NO>> JoinNetwork<NI>
+    for Reinforcement<NI, NO, N>
+{
+}
+impl<const NI: usize, const NO: usize, N: Network<NI, NO>> ForwardNetwork<NI, NO>
+    for Reinforcement<NI, NO, N>
+{
+    fn forward(&mut self, input: [Float; NI]) -> [Float; NO] {
+        let output = self.network.forward(input);
+        self.network
+            .update_gradient(self.r, output.map(|v| v * self.emphasis));
+        output
+    }
+}
+impl<const NI: usize, const NO: usize, N: Network<NI, NO>> Network<NI, NO>
+    for Reinforcement<NI, NO, N>
+{
+    fn update_gradient(&mut self, r: Float, delta: [Float; NO]) -> [Float; NI] {
+        self.network.update_gradient(r, delta)
+    }
+
+    fn reset_gradient(&mut self) {
+        self.network.reset_gradient();
+    }
+
+    fn apply_gradient(&mut self, alpha: Float) {
+        self.network.apply_gradient(alpha);
     }
 }
