aeb2727cd56e40068ffe726168495c057decd49f
xayon40-12 <touroux.nathan@gmail.com>
2025-07-04 22:12:23 +0200

    try to use iced for rendering

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use std::time::Duration;
+
 use array_vector_space::ArrayVectorSpace;
+use iced::widget::{column, container, iced, slider, text, vertical_slider};
+use iced::{Center, Element, Fill, Subscription, time};
 use reinforcement::network::{
     Float, Network,
     activation::{Id, Relu, SigmoidSim},
@@ -38,99 +42,161 @@ fn xor() {
     );
 }
 
-pub fn distance(a: [Float; 2], b: [Float; 2]) -> Float {
-    ((a[0] - b[0]).powi(2) + (a[1] - b[1]).powi(2)).sqrt()
+pub fn main() -> iced::Result {
+    add();
+    xor();
+
+    iced::application("Test", Slider::update, Slider::view)
+        .subscription(Slider::subscription)
+        .run()
 }
 
-fn reinforcement() {
-    let mut net: Reinforcement<
-        6,
-        2,
-        Layers<6, 10, Relu, Layers<10, 10, Relu, Layer<10, 2, SigmoidSim>>>,
-    > = Default::default();
-    net.randomize();
+#[derive(Debug, Clone)]
+pub enum Message {
+    SliderChanged(u8),
+    Tick,
+}
 
-    let alpha = 1e-3;
-    let relaxation = 1e-3;
-    let shape = 30.0;
+pub struct Slider {
+    value: u8,
+    net: Reinforcement<6, 2, Layers<6, 10, Relu, Layers<10, 10, Relu, Layer<10, 2, SigmoidSim>>>>,
+    alpha: Float,
+    relaxation: Float,
+    shape: Float,
+    obstacles_radius: Float,
+    targets: [[Float; 2]; 4],
+    rewards: [Reward; 4],
+    start: [Float; 2],
+    nb_reinforcement: usize,
+    ds: [Float; 4],
+    dtots: [Float; 4],
+    trajectory: [[[Float; 2]; 4]; MAX_TICKS],
+}
 
-    const MAX_TICKS: usize = 200;
-    let nb_reinforcement = 1000000usize;
+const MAX_TICKS: usize = 200;
+impl Slider {
+    fn new() -> Self {
+        let mut s = Slider {
+            value: 50,
+            net: Default::default(),
+            alpha: 1e-3,
+            relaxation: 1e-3,
+            shape: 30.0,
+            obstacles_radius: 40.0,
+            targets: [
+                [100.0 as Float, 100.0],
+                [100.0, -100.0],
+                [-100.0, -100.0],
+                [-100.0, 100.0],
+            ],
+            start: [0.0; 2],
+            rewards: [Reward::default(); 4],
+            nb_reinforcement: 50,
+            ds: [0.0; 4],
+            dtots: [0.0; 4],
+            trajectory: [[[0.0; 2]; 4]; MAX_TICKS],
+        };
+        s.net.randomize();
+        s
+    }
+    fn subscription(&self) -> Subscription<Message> {
+        // 16 ms ≈ 60 Hz
+        time::every(Duration::from_millis(16)).map(|_| Message::Tick)
+    }
 
-    let targets = [
-        [100.0 as Float, 100.0],
-        [100.0, -100.0],
-        [-100.0, -100.0],
-        [-100.0, 100.0],
-    ];
-    let obstacle = 40.0;
-    let start = [0.0; 2];
-    let mut rewards = targets.map(|_| Reward::default());
-
-    let width = nb_reinforcement.ilog10() as usize;
-    println!("{:?}", targets.map(|[x, y]| (x * x + y * y).sqrt()));
-
-    for i in 1..nb_reinforcement {
-        let mut poss = targets.map(|_| (start, [0.0; 2]));
-        let mut dtots = targets.map(|_| 0.0);
-        let mut ds = targets.map(|_| 0.0);
-        let mut trajectory = [[[0.0; 2]; 4]; MAX_TICKS];
-        for (((((target, (pos, speed)), dtot), d), reward), j) in targets
-            .iter()
-            .zip(poss.iter_mut())
-            .zip(dtots.iter_mut())
-            .zip(ds.iter_mut())
-            .zip(rewards.iter_mut())
-            .zip(0..)
-        {
-            let mut ticks = 0;
-            net.reinforce(relaxation, alpha, |net| {
-                let input = [pos[0], pos[1], speed[0], speed[1], target[0], target[1]];
-                let output = net.pert_forward(input, shape);
-                *speed = speed.add(output.scal_mul(1e-1));
-                let next_pos = pos.add(*speed);
-                if (0..4)
-                    .map(|i| {
-                        next_pos.sub(targets[i].scal_mul(0.5)).norm2() < (obstacle as Float).powi(2)
-                    })
-                    .fold(false, |a, b| a || b)
-                {
-                    *speed = [0.0; 2];
-                } else {
-                    *pos = next_pos;
-                }
-                trajectory[ticks][j] = *pos;
-                ticks += 1;
-                *dtot += speed.norm2().sqrt();
-                if ticks >= MAX_TICKS {
-                    *d = distance(*target, *pos);
-                    Some(reward.update(-*dtot / (1.0 + *d) * 1e-3 - *d))
-                } else {
-                    None
-                }
-            });
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::SliderChanged(value) => {
+                self.value = value;
+            }
+            Message::Tick => {
+                self.reinforce();
+            }
         }
-        if i % (nb_reinforcement / 100) == 0 {
-            std::fs::write(
-                "tmp_trajectory.txt",
-                trajectory
-                    .map(|vs| vs.map(|v| v.map(|v| v.to_string()).join(" ")).join(" "))
-                    .join("\n"),
-            )
-            .ok();
-            println!(
-                "{i:<width$} {MAX_TICKS:<3}: target dist {:.1} {:?} | tot dist {:.1} {:?}",
-                ds.iter().fold(0.0 as Float, |a, v| a + v * v).sqrt(),
-                ds.map(|v| v as i64),
-                dtots.iter().fold(0.0 as Float, |a, v| a + v * v).sqrt(),
-                dtots.map(|v| v as i64)
-            );
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        let h_slider = container(
+            slider(1..=100, self.value, Message::SliderChanged)
+                .default(50)
+                .shift_step(5),
+        )
+        .width(250);
+
+        let v_slider = container(
+            vertical_slider(1..=100, self.value, Message::SliderChanged)
+                .default(50)
+                .shift_step(5),
+        )
+        .height(200);
+
+        let text = text(format!(
+            "{MAX_TICKS:<3}: target dist {:.1} {:?} | tot dist {:.1} {:?}",
+            self.ds.iter().fold(0.0 as Float, |a, v| a + v * v).sqrt(),
+            self.ds.map(|v| v as i64),
+            self.dtots
+                .iter()
+                .fold(0.0 as Float, |a, v| a + v * v)
+                .sqrt(),
+            self.dtots.map(|v| v as i64)
+        ));
+
+        column![v_slider, h_slider, text, iced(self.value as f32),]
+            .width(Fill)
+            .align_x(Center)
+            .spacing(20)
+            .padding(20)
+            .into()
+    }
+
+    fn reinforce(&mut self) {
+        for _ in 0..self.nb_reinforcement {
+            let mut poss = self.targets.map(|_| (self.start, [0.0; 2]));
+            self.dtots = self.targets.map(|_| 0.0);
+            self.ds = self.targets.map(|_| 0.0);
+            for (((((target, (pos, speed)), dtot), d), reward), j) in self
+                .targets
+                .iter()
+                .zip(poss.iter_mut())
+                .zip(self.dtots.iter_mut())
+                .zip(self.ds.iter_mut())
+                .zip(self.rewards.iter_mut())
+                .zip(0..)
+            {
+                let mut ticks = 0;
+                self.net.reinforce(self.relaxation, self.alpha, |net| {
+                    let input = [pos[0], pos[1], speed[0], speed[1], target[0], target[1]];
+                    let output = net.pert_forward(input, self.shape);
+                    *speed = speed.add(output.scal_mul(1e-1));
+                    let next_pos = pos.add(*speed);
+                    if (0..4)
+                        .map(|i| {
+                            next_pos.sub(self.targets[i].scal_mul(0.5)).norm2()
+                                < (self.obstacles_radius as Float).powi(2)
+                        })
+                        .fold(false, |a, b| a || b)
+                    {
+                        *speed = [0.0; 2];
+                    } else {
+                        *pos = next_pos;
+                    }
+                    self.trajectory[ticks][j] = *pos;
+                    ticks += 1;
+                    *dtot += speed.norm2().sqrt();
+                    if ticks >= MAX_TICKS {
+                        *d = target.sub(*pos).norm2().sqrt();
+                        Some(reward.update(-*dtot / (1.0 + *d) * 1e-3 - *d))
+                    } else {
+                        None
+                    }
+                });
+            }
         }
     }
 }
 
-fn main() {
-    add();
-    xor();
-    reinforcement();
+impl Default for Slider {
+    fn default() -> Self {
+        Self::new()
+    }
 }
