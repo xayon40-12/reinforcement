2a51140d721811a38bc126e1e991040865d7026e
xayon40-12 <touroux.nathan@gmail.com>
2025-07-05 10:50:28 +0200

    working egui rendering

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::time::Duration;
-
 use array_vector_space::ArrayVectorSpace;
-use iced::widget::{column, container, iced, slider, text, vertical_slider};
-use iced::{Center, Element, Fill, Subscription, time};
+use eframe::{CreationContext, egui};
+use egui::{
+    Color32, Frame, Key, Pos2, Rect, ScrollArea, emath,
+    epaint::{self, PathStroke},
+    lerp, pos2, remap, vec2,
+};
 use reinforcement::network::{
     Float, Network,
     activation::{Id, Relu, SigmoidSim},
@@ -10,6 +12,8 @@ use reinforcement::network::{
     layers::Layers,
     reinforcement::{Reinforcement, Reward},
 };
+
+const MAX_TICKS: usize = 200;
 
 fn add() {
     //TODO use identity for activation for this case
@@ -42,113 +46,108 @@ fn xor() {
     );
 }
 
-pub fn main() -> iced::Result {
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result {
     add();
     xor();
 
-    iced::application("Test", Slider::update, Slider::view)
-        .subscription(Slider::subscription)
-        .run()
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Reinforcement",
+        native_options,
+        Box::new(|_cc| Ok(Box::new(Content::new()))),
+    )
 }
 
-#[derive(Debug, Clone)]
-pub enum Message {
-    SliderChanged(u8),
-    Tick,
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|_cc| Ok(Box::new(Content::new()))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
 
-pub struct Slider {
-    value: u8,
+struct Content {
     net: Reinforcement<6, 2, Layers<6, 10, Relu, Layers<10, 10, Relu, Layer<10, 2, SigmoidSim>>>>,
     alpha: Float,
     relaxation: Float,
     shape: Float,
-    obstacles_radius: Float,
+    obstacles: [([Float; 2], Float); 4],
     targets: [[Float; 2]; 4],
     rewards: [Reward; 4],
     start: [Float; 2],
     nb_reinforcement: usize,
     ds: [Float; 4],
     dtots: [Float; 4],
-    trajectory: [[[Float; 2]; 4]; MAX_TICKS],
+    trajectory: [[[Float; 2]; MAX_TICKS]; 4],
 }
 
-const MAX_TICKS: usize = 200;
-impl Slider {
+impl Content {
     fn new() -> Self {
-        let mut s = Slider {
-            value: 50,
+        let targets = [
+            [100.0 as Float, 100.0],
+            [100.0, -100.0],
+            [-100.0, -100.0],
+            [-100.0, 100.0],
+        ];
+        let mut s = Content {
             net: Default::default(),
             alpha: 1e-3,
             relaxation: 1e-3,
             shape: 30.0,
-            obstacles_radius: 40.0,
-            targets: [
-                [100.0 as Float, 100.0],
-                [100.0, -100.0],
-                [-100.0, -100.0],
-                [-100.0, 100.0],
-            ],
+            obstacles: targets.map(|t| (t.scal_mul(0.5), 40.0)),
+            targets,
             start: [0.0; 2],
             rewards: [Reward::default(); 4],
-            nb_reinforcement: 50,
+            nb_reinforcement: 166,
             ds: [0.0; 4],
             dtots: [0.0; 4],
-            trajectory: [[[0.0; 2]; 4]; MAX_TICKS],
+            trajectory: [[[0.0; 2]; MAX_TICKS]; 4],
         };
         s.net.randomize();
         s
     }
-    fn subscription(&self) -> Subscription<Message> {
-        // 16 ms ≈ 60 Hz
-        time::every(Duration::from_millis(16)).map(|_| Message::Tick)
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::SliderChanged(value) => {
-                self.value = value;
-            }
-            Message::Tick => {
-                self.reinforce();
-            }
-        }
-    }
-
-    fn view(&self) -> Element<'_, Message> {
-        let h_slider = container(
-            slider(1..=100, self.value, Message::SliderChanged)
-                .default(50)
-                .shift_step(5),
-        )
-        .width(250);
-
-        let v_slider = container(
-            vertical_slider(1..=100, self.value, Message::SliderChanged)
-                .default(50)
-                .shift_step(5),
-        )
-        .height(200);
-
-        let text = text(format!(
-            "{MAX_TICKS:<3}: target dist {:.1} {:?} | tot dist {:.1} {:?}",
-            self.ds.iter().fold(0.0 as Float, |a, v| a + v * v).sqrt(),
-            self.ds.map(|v| v as i64),
-            self.dtots
-                .iter()
-                .fold(0.0 as Float, |a, v| a + v * v)
-                .sqrt(),
-            self.dtots.map(|v| v as i64)
-        ));
-
-        column![v_slider, h_slider, text, iced(self.value as f32),]
-            .width(Fill)
-            .align_x(Center)
-            .spacing(20)
-            .padding(20)
-            .into()
-    }
-
     fn reinforce(&mut self) {
         for _ in 0..self.nb_reinforcement {
             let mut poss = self.targets.map(|_| (self.start, [0.0; 2]));
@@ -169,18 +168,17 @@ impl Slider {
                     let output = net.pert_forward(input, self.shape);
                     *speed = speed.add(output.scal_mul(1e-1));
                     let next_pos = pos.add(*speed);
-                    if (0..4)
-                        .map(|i| {
-                            next_pos.sub(self.targets[i].scal_mul(0.5)).norm2()
-                                < (self.obstacles_radius as Float).powi(2)
-                        })
+                    if self
+                        .obstacles
+                        .into_iter()
+                        .map(|(p, r)| next_pos.sub(p).norm2() < r * r)
                         .fold(false, |a, b| a || b)
                     {
                         *speed = [0.0; 2];
                     } else {
                         *pos = next_pos;
                     }
-                    self.trajectory[ticks][j] = *pos;
+                    self.trajectory[j][ticks] = *pos;
                     ticks += 1;
                     *dtot += speed.norm2().sqrt();
                     if ticks >= MAX_TICKS {
@@ -194,9 +192,69 @@ impl Slider {
         }
     }
 }
+impl eframe::App for Content {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            self.reinforce();
+            ui.horizontal(|ui| {
+                if ui.button("reset").clicked() {
+                    *self = Self::new();
+                }
+                ui.label(format!(
+                    "{MAX_TICKS:<3}: target dist {:.1} {:?} | tot dist {:.1} {:?}",
+                    self.ds.iter().fold(0.0 as Float, |a, v| a + v * v).sqrt(),
+                    self.ds.map(|v| v as i64),
+                    self.dtots
+                        .iter()
+                        .fold(0.0 as Float, |a, v| a + v * v)
+                        .sqrt(),
+                    self.dtots.map(|v| v as i64)
+                ));
+            });
 
-impl Default for Slider {
-    fn default() -> Self {
-        Self::new()
+            Frame::canvas(ui.style()).show(ui, |ui| {
+                let desired_size = ui.available_size();
+                let ratio = desired_size.x / desired_size.y;
+                let (_id, rect) = ui.allocate_space(desired_size);
+
+                let to_screen = emath::RectTransform::from_to(
+                    Rect::from_x_y_ranges(-200.0 * ratio..=200.0 * ratio, -200.0..=200.0),
+                    rect,
+                );
+                let colors = [
+                    Color32::from_rgb(255, 0, 0),
+                    Color32::from_rgb(0, 255, 0),
+                    Color32::from_rgb(0, 0, 255),
+                    Color32::from_rgb(255, 255, 0),
+                ];
+                let shapes = self
+                    .trajectory
+                    .iter()
+                    .zip(colors.into_iter())
+                    .map(|(t, c)| {
+                        epaint::Shape::line(
+                            t.map(|[x, y]| to_screen * pos2(x as _, y as _)).to_vec(),
+                            PathStroke::new(5.0, c),
+                        )
+                    })
+                    .chain(self.obstacles.into_iter().map(|([x, y], r)| {
+                        epaint::Shape::circle_filled(
+                            to_screen * pos2(x as _, y as _),
+                            to_screen.scale().y * r as f32,
+                            Color32::BLACK,
+                        )
+                    }))
+                    .chain(self.targets.into_iter().map(|[x, y]| {
+                        epaint::Shape::circle_filled(
+                            to_screen * pos2(x as _, y as _),
+                            to_screen.scale().y * 5.0,
+                            Color32::GRAY,
+                        )
+                    }));
+
+                ui.painter().extend(shapes);
+            });
+        });
+        ctx.request_repaint();
     }
 }
